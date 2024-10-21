758c018aa74b5aa1e3cdcecdda2ebea76d7fe1fe
Fayvara21 <thelogane974@gmail.com>
2024-10-21 15:25:39 +0200

    api_request terminé

--- api_request.php
+++ api_request.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,15 +27,81 @@
          $response = curl_exec($ch);
     
         curl_close($ch);      
-        echo $response;
+        //echo $response;
         $data = json_decode($response, true);
         foreach ($data as $x) {
-            echo "<p style='width:100px;height:100px;background-color:#20FFFF;'>".$x['fullName']."<p>";
-          }
+            // AJOUTE LES UTILISATEURS à LA BASE DE DONNEE:
+            echo $x['id'].'  ';
+        }
+    }
+    function getCards(){
+        global $ch;
+        global $APIId;
+        global $APIKey;
+        global $APIToken;
+        
+        $cards = array();
+
+        $url = 'https://api.trello.com/1/boards/'.$APIId.'/cards?key='.$APIKey.'&token='.$APIToken;
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
+        curl_setopt($ch, CURLOPT_URL, $url);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+         $response = curl_exec($ch);
+    
+        curl_close($ch);      
+        //echo $response;
+        $data = json_decode($response, true);
+        foreach ($data as $x){
+            if (!empty($x['idMembers']['0']) and !empty($x['idMembers']['1'])){
+                echo 'nom: '.$x['name']
+                .', idTache:'.$x['id']
+                .', délégué:'.$x['idMembers']['0']
+                .', assisté par:'.$x['idMembers']['1']
+                .'           ';
+            }
+            else{
+                echo 'nom: '.$x['name']
+                .', délégué/assisté incomplet'
+                .'           ';
+            }
+
+            $cards[] = $x['id'];
+
+        }
+        return $cards;
     }
     
-    getMembers();
+    function setCard($cardID, $listUserID){
+        global $ch;
+        global $APIKey;
+        global $APIToken;
+        
+        $url = 'https://api.trello.com/1/cards/'.$cardID.'?key='.$APIKey.'&token='.$APIToken;
 
+        $data = array("idMembers" => $listUserID);
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); 
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));  // Send data as JSON    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',  // Set content type to JSON
+            'Content-Length: ' . strlen(json_encode($data))
+        ));
+    
+        $response = curl_exec($ch);
+        
+        curl_close($ch);      
+        echo $response;
+
+
+    }
+
+    $cardList = getCards();
+    setCard( $cardList[0], array('663a357f099cc40c27b3ea8f', '670cda89a1a8d52b5c88721a'));
 
 
 
