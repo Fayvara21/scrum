f20d905ba76399d0dbbbfa72836abad7df27b82a
Fayvara21 <thelogane974@gmail.com>
2024-10-21 11:27:21 +0200

    api_request

--- api_request.php
+++ api_request.php
@@ -12,20 +12,35 @@
     $APIToken = "ATTA949bbd3340a9de00e9bf136eed5672c00636bcebfff1aaa7452ab178eccc2cedD4F24C3D";
     $APIKey = "0b2e7b2c9467bca4b281573eb177b77c";
     $APIId = "670cdbb802e7d153f9f5553f";
-    $url = 'https://api.trello.com/1/boards/'.$APIId.'/members?key='.$APIKey.'&token='.$APIToken;
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
-	curl_setopt($ch, CURLOPT_URL, $url);
 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    function getMembers(){
+        global $ch;
+        global $APIId;
+        global $APIKey;
+        global $APIToken;
 
-     $response = curl_exec($ch);
+        $url = 'https://api.trello.com/1/boards/'.$APIId.'/members?key='.$APIKey.'&token='.$APIToken;
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET"); 
+        curl_setopt($ch, CURLOPT_URL, $url);
+    
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+         $response = curl_exec($ch);
+    
+        curl_close($ch);      
+        echo $response;
+        $data = json_decode($response, true);
+        foreach ($data as $x) {
+            echo "<p style='width:100px;height:100px;background-color:#20FFFF;'>".$x['fullName']."<p>";
+          }
+    }
+    
+    getMembers();
 
-	curl_close($ch);      
-    echo $response;
-    $data = json_decode($response, true);
-    foreach ($data as $x) {
-        echo "<p style='width:100px;height:100px;background-color:#20FFFF;'>".$x['fullName']."<p>";
-      }
+
+
+
+
 ?>
 </body>
 </html>
