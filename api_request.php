0c737d95c53f0003191a274506fbc6ff294b55a2
Fayvara21 <thelogane974@gmail.com>
2024-10-21 09:39:22 +0200

    api

--- api_request.php
+++ api_request.php
@@ -8,19 +8,22 @@
 </head>
 <body>
 <?php
+	$ch = curl_init();
 
-$ch = curl_init("curl 'https://api.trello.com/1/boards/670cdbb802e7d153f9f5553f?key=0b2e7b2c9467bca4b281573eb177b77c&token=ATTA949bbd3340a9de00e9bf136eed5672c00636bcebfff1aaa7452ab178eccc2cedD4F24C3D'");
-$fp = fopen("example_homepage.txt", "w");
+	curl_setopt($ch, CURLOPT_URL, "https://api.trello.com/1/boards/670cdbb802e7d153f9f5553f?key=0b2e7b2c9467bca4b281573eb177b77c&token=ATTA949bbd3340a9de00e9bf136eed5672c00636bcebfff1aaa7452ab178eccc2cedD4F24C3D");
 
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-curl_exec($ch);
-if(curl_error($ch)) {
-    fwrite($fp, curl_error($ch));
-}
-curl_close($ch);
-fclose($fp); ?>
+     $response = curl_exec($ch);
+
+
+	curl_close($ch);      
+
+	echo $response;
+
+
+
+?>
 </body>
 </html>
 
