3386018bc200730bc9206193ca37f9ec71a07476
Fayvara21 <thelogane974@gmail.com>
2024-10-22 09:52:52 +0200

    k

--- php/inscription.php
+++ php/inscription.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,39 +8,40 @@
   <link rel="stylesheet" href="../css/connexion.css">
   <title>Document</title>
 </head>
+
 <body>
 
 
   <?php
-    require_once './php/db_connect.php';
-    if (isset($_SESSION['user'])) {
-        header('Location: ../index.html');
+  require_once './php/db_connect.php';
+  if (isset($_SESSION['user'])) {
+    header('Location: ../index.html');
+  }
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $query = "SELECT * FROM users WHERE username = :username AND password = :password";
+    $statement = $pdo->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':password', $password);
+    $statement->execute();
+    $user = $statement->fetch();
+    if ($user) {
+      session_start();
+      $_SESSION['user'] = $user;
+      header('Location: ../index.html');
+    } else {
+      $error = 'Nom d\'utilisateur ou mot de passe incorrect';
     }
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $query = "SELECT * FROM users WHERE username = :username AND password = :password";
-        $statement = $pdo->prepare($query);
-        $statement->bindValue(':username', $username);
-        $statement->bindValue(':password', $password);
-        $statement->execute();
-        $user = $statement->fetch();
-        if ($user) {
-            session_start();
-            $_SESSION['user'] = $user;
-            header('Location: ../index.html');
-        } else {
-            $error = 'Nom d\'utilisateur ou mot de passe incorrect';
-        }
 
-    }
-    
-?>
+  }
+
+  ?>
 
 
   <!-- LA BARRE DU HAUT, CHANGER LE HREF AVEC LES AUTRES PAGES-->
-  
+
   <div class="topBar">
     <p titre>SCRUM</p>
     <p newline></p>
@@ -51,26 +53,47 @@
 
   <!-- LE CONTENU DE LA PAGE-->
   <div class="content">
- 
 
 
-  <!-- PAGE DE CONNEXION-->
 
-  <div class="connexion">
-    <form action="connect_user.php" method="post">
+    <!-- PAGE DE CONNEXION-->
+
+    <div class="connexion">
+      <form action="register_user.php" method="post">
+
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" name="nom" required>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom" name="prenom" required>
+
+        <label for="pseudo">Pseudo :</label>
+        <input type="text" id="pseudo" name="pseudo" required>
+
         <label for="email">Email :</label>
-        <input type="email" id="email" class="input" name="email" required>
-        <p space></p>
+        <input type="email" id="email" name="email" required>
+
         <label for="mot_de_passe">Mot de passe :</label>
-        <input type="password" id="mot_de_passe" class="input" name="mot_de_passe" required>
-        <p space></p>
-        <button class="Button" type="submit">Se connecter</button>
-        <a class="#" href="db_register.php" target="_blank" >Inscrivez-vous</a>
-        
+        <input type="password" id="password" name="password" required>
+
+        <label for="anniversaire">Date d'anniversaire :</label>
+        <input type="date" id="anniversaire" name="anniversaire" required>
+
+        <div class="input-field col s6">
+          <i class="material-icons prefix">phone</i>
+          <input type="text" name="telephone" maxlength="10">
+
+
+        </div>
+
+        <button class="ombres_multiples_diffuses" type="submit">S'inscrire</button>
+        <p>ou</p>
+        <a href="home.php" target="_blank">Vous avez déjâ un compte</a>
       </form>
-  </div>
+    </div>
 
   </div>
 
 </body>
+
 </html>
--- php/register_user.php
+++ php/register_user.php
@@ -1,3 +1,8 @@
+<?php
+
+require_once '../php/db_connect.php';
+
+
 $prenom = $_POST['prenom'];
 $nom = $_POST['nom'];
 $pseudo = $_POST['pseudo'];
@@ -5,15 +10,12 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $anniv = $_POST['anniversaire'];
 $tel = $_POST['telephone'];
-try 
-{
+try {
     $password = password_hash($password, PASSWORD_DEFAULT);
     $sql = " INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `profile_picture`, `email`, `password`, `birthday`, `phone`, `active`) VALUES (NULL, '$prenom', '$nom', '$pseudo', '', '$email', '$password', '$anniv', '$tel', '1')";
     $pdo->exec($sql);
     echo "Nouveau compte créer";
-} 
-catch(PDOException $e) 
-{
+} catch (PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }
-$conn = null
+$conn = null;
