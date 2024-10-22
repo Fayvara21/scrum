e1d82aa316fa7a309d40589b329a0e8c975f63eb
Fayvara21 <thelogane974@gmail.com>
2024-10-22 15:22:50 +0200

    j

--- php/connexion.php
+++ php/connexion.php
@@ -14,7 +14,6 @@
     if (isset($_SESSION['user'])) {
       header('Location: ../index.html');
     }
-    require_once '../php/connect_user.php';
   ?>
 
   <!-- LA BARRE DU HAUT, CHANGER LE HREF AVEC LES AUTRES PAGES-->
