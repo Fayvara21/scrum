5d36772d5d96b99423681fc34d69e1717e33d211
Fayvara21 <thelogane974@gmail.com>
2024-10-23 10:21:42 +0200

    j

--- php/removeuser.php
+++ php/removeuser.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
         //echo $_SESSION['user_recieve'].$_SESSION['user']['id']   ;
         //header('Location: gerer_groupe.php');
-        addMember("670cdbb802e7d153f9f5553f", $_POST['user']);
+        deleteMember("670cdbb802e7d153f9f5553f", $_POST['user']);
 
 
 }
