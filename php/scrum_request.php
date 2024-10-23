b8c4cc7d4eb358bf8ca2715d0df9b2d4891af428
Fayvara21 <thelogane974@gmail.com>
2024-10-23 12:09:12 +0200

    cartes fonctionnelles

---
+++ php/scrum_request.php
@@ -0,0 +1,13 @@
+<?php 
+
+require_once 'db_connect.php';
+require 'api_request.php';
+
+
+$conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    descCard('670d071bffdca2977a959c05', $_POST['button']);
+
+}
