362d6336a23ca581175289f1be9e5a628caf27dd
Fayvara21 <thelogane974@gmail.com>
2024-10-25 11:43:54 +0200

    j

--- php/connect_user.php
+++ php/connect_user.php
@@ -14,15 +14,15 @@
             session_start();
             $_SESSION['user'] = $user;
             echo $_SESSION['user'];
-            //echo"session started";
+            echo"session started";
             header('Location: ../index.html');
         } else {
-            //$error = 'Nom d\'utilisateur ou mot de passe incorrect';
-            //echo $error;
+            $error = 'Nom d\'utilisateur ou mot de passe incorrect';
+            echo $error;
         }
 
     } catch (PDOException $e) {
-        //echo $sql . "<br>" . $e->getMessage();
+        echo $sql . "<br>" . $e->getMessage();
     }
     
         
