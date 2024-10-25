d6f0c7f2972246b1615c8c570f9bf76de9ada44f
Fayvara21 <thelogane974@gmail.com>
2024-10-25 12:11:18 +0200

    j

--- php/scrum_request.php
+++ php/scrum_request.php
@@ -7,9 +7,14 @@ require 'api_request.php';
 $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+session_start();
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $desc = getDesc($_POST['card']);
     $select = $_POST['select'];
+
+
     // if ((int)$desc && (int)$_POST['card']){
         // if ((int)$desc < (int)$_POST['card']){
              //putDesc($_POST['card'], $_POST['select']);
